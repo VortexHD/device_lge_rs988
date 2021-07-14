74b4d157d9100e1ac791f46db3be67aeaa7ca762
VortexHD <vortexhdwhite@gmail.com>
2021-07-14 17:18:16 +0100

    Revert "rs988: Add Gcam Go"
    This reverts commit 3c06d3aa06924f29ecf717ae47825a78cb1ea1f0.

--- device.mk
+++ device.mk
@@ -36,8 +36,3 @@ PRODUCT_COPY_FILES += \
 # WiFi Calibration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal
-
-# GCam Go
-PRODUCT_PACKAGES += \
-    GCam
-
