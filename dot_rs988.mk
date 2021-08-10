8a8e05579674b527d9341ac5a4f3bba0787253f9
VortexHD <vortexhdwhite@gmail.com>
2021-08-10 16:40:12 +0100

    rs988: Fix fp mistake

--- dot_rs988.mk
+++ dot_rs988.mk
@@ -40,6 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="h1" \
     PRODUCT_DEVICE="h1" \
     PRODUCT_NAME="h1_lra_us" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001 7474174 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
